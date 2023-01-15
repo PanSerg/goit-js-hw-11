787a0213b47bb576bb4ec991b19154f5690a35c1
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-01-15 13:41:59 +0100

    task 1.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-// import cardTemplate from '../src/card-template.hbs';
+import fetchImages from './fetch-images';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const searchForm = document.querySelector('#search-form');
@@ -10,10 +10,47 @@ const loadMoreBtn = document.querySelector('.load-more');
 //   'https://pixabay.com/api/?key=32820375-2ad81e3b731d859a46f31c661&q=&image_type=photo&orientation=horizontal&safesearch=true'
 // ).then(response => console.log(response));
 
+// function renderCardImg(arr) {
+//     const markup = arr.map(item => cardTemplate(item)).join('');
+//     gallery.insertAdjacentHTML('beforeend', markup);
+//   };
+
 function renderCardImg(arr) {
-    const markup = arr.map(item => cardTemplate(item)).join('');
-    gallery.insertAdjacentHTML('beforeend', markup);
-  };
+    const markup = arr.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads
+    }) => {
+        return `<div class='photo-card'>
+                    <a href='{{largeImageURL}}'>
+                        <img src={{webformatURL}}' alt='{{tags}}' loading='lazy' />
+                    </a>
+                    <div class='info'>
+                        <p class='info-item'>
+                             <b>Likes</b>
+                                {{likes}}
+                        </p>
+                        <p class='info-item'>
+                            <b>Views</b>
+                             {{views}}
+                        </p>
+                        <p class='info-item'>
+                             <b>Comments</b>
+                                {{comments}}
+                        </p>
+                        <p class='info-item'>
+                            <b>Downloads</b>
+                            {{downloads}}
+                        </p>
+                    </div>
+                </div>`;
+    }).join('');
+    return markup;
+}
 
 let currentPage = 1;
 let currentHits = 0;
@@ -30,7 +67,7 @@ async function onSubmit(evt) {
         return;
     }
 
-    const response = await fetchImg(searchQuery, currentPage);
+    const response = await fetchImages(searchQuery, currentPage);
     currentHits = response.hits.length;
 
     if (response.totalHits > 40) {
